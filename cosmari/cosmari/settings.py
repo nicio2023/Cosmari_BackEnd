99b1625032ceb0588d51ea1747150084a032d380
OmarEli92 <omar.elidrissi@studenti.unicam.it>
2025-02-18 16:17:41 +0100

    fixed bugs

--- cosmari/cosmari/settings.py
+++ cosmari/cosmari/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "cosmari.backend.middlewares.JWTAuthMiddleware",
+    "backend.middlewares.JWTAuthMiddleware",
 ]
 
 ROOT_URLCONF = 'cosmari.urls'
