8f2c3cf1f689c9f417586268f1cbd220b818a3e3
nicio2023 <nicolas.rossi@studenti.unicam.it>
2025-02-22 16:31:56 +0100

    bug fixed

--- cosmari/api_bucher/urls.py
+++ cosmari/api_bucher/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from .views import (get_api_token, get_assets_tracking, get_mission_data)
 
 urlpatterns = [
-    path('token-boucher/', get_api_token, name='token-boucher'),
+    path('token-bucher/', get_api_token, name='token-bucher'),
     path('mission/', get_mission_data, name='mission'),
     path('assets-tracking/', get_assets_tracking, name="assets")
-
 ]
